e22e5bdc1959c881dbc752c066b62c4b17e8da97
aFraley <humanimalmammal@webtv.net>
2016-10-08 21:43:30 -0500

    improved html form

--- assets/dev/js/main.js
+++ assets/dev/js/main.js
@@ -6,14 +6,20 @@
 */
 
 function roll(){
+
+			// Sort of our Model
+			//========================================================================
 			var dice = document.getElementById("dice").value;
 			var sides = document.getElementById("sides").value;
-			var e = document.getElementById("posNeg").value;
 			var mod = 0;
 			var d20 = document.getElementById("d20");
 			var out = "Roll " + dice + "d" + sides + " [";
 			var sum = 0;
-			//rolls dice, invokes loop if more than one die are rolled
+
+			// Kind of our Controller
+			// Roll dice.
+			// Loop to roll more than one dice, if dice > 1
+			//========================================================================
 			for ( var i = dice; i > 0; i--) {
 				var roll = Math.floor(Math.random() * sides) +1;
 					sum += roll;
@@ -24,33 +30,33 @@ function roll(){
 						out += "";
 					}
 
-					//determines mod, value of sum and mod
+					// Apply modifier
 					mod = document.getElementById("mod").value;
-					if (e == "+") {
-						sum += 1 * mod;
-						out += " + ";
-					}else if (e == "-"){
-						sum -= 1 * mod;
-						out += " - ";
+					sum += 1 * mod;
+					if (mod >= 0) {
+						out += "+";
 					}
-
 				}
-				//final output
+
+				// Sort of our View
+				// Output results.
+				//======================================================================
 				d20.value = sum;
-				out += mod + " = " + sum + "\n";
+				out += mod + "=" + sum + "\n\n";
 				var results = document.getElementById("results");
 				results.value = out + results.value;
 		}
-		//resets all fields
+
+		// Reset all fields.
 		function reset() {
 			var sides = document.getElementById("sides").value = 20;
 			var mod = document.getElementById("mod")
 			var dice = document.getElementById("dice").value = 1;
 			var d20 = document.getElementById("d20").value = "";
 			var results = document.getElementById("results").value = "";
-
 		}
-		//clears text area
+
+		// Clear text area that functions as an in document log for rolls.
 		function clearResults() {
 			var results = document.getElementById("results").value = "";
 			var d20 = document.getElementById("d20").value = "";
